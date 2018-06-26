9cf20666f8d6602761b815a33d414079f189043f
sumit <mehrasumit258@gmail.com>
2018-06-26 17:58:11 +0530

    Checking Ionic PRo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,13 +18,13 @@ export class MyApp {
   // rootPage: any = LoginPage;
   // rootPage: any = HomePage;
   // rootPage: any = ProductPage;
-  rootPage: any = PerfumePage;
+  rootPage: any = HomePage;
 
   pages: Array<{title: string, component: any}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
-
+// TODO CHeck this
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
